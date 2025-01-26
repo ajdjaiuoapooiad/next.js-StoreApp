e7a6232f6a4cb80608cf8909d2ccf044b33d2b0c
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-26 23:03:32 +0900

    ok

---
+++ components/navbar/LinksDropdowm.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LinksDropdowm = () => {
+  return (
+    <div>LinksDropdowm</div>
+  )
+}
+
+export default LinksDropdowm
---
+++ components/navbar/SignoutLink.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SignoutLink = () => {
+  return (
+    <div>SignoutLink</div>
+  )
+}
+
+export default SignoutLink
