f6123d15edb68f0c2fdfac3b1091767be3906a0c
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-27 08:54:58 +0900

    ok

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Link from 'next/link';
+import { Button } from '../ui/button';
+import { LuArmchair } from 'react-icons/lu';
+import { VscCode } from 'react-icons/vsc';
 
-const Logo = () => {
+function Logo() {
   return (
-    <div>Logo</div>
-  )
+    <Button size='icon' asChild>
+      <Link href='/'>
+        <VscCode className='w-6 h-6' />
+      </Link>
+    </Button>
+  );
 }
 
-export default Logo
+export default Logo;
