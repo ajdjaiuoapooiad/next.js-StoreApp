671fadb052631b33c81b3e25dccb1e9a817e499a
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-27 14:34:36 +0900

    ok

--- components/home/FeaturedProducts.tsx
+++ components/home/FeaturedProducts.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { fetchFeaturedProducts } from '@/utils/actions';
+import EmptyList from '../global/EmptyList';
+import SectionTitle from '../global/SectionTitle';
+import ProductsGrid from '../products/ProductsGrid';
 
-const FeaturedProducts = () => {
-  return (
-    <div>FeaturedProducts</div>
-  )
+
+
+async function FeaturedProducts() {
+  const products = await fetchFeaturedProducts();
+    if (products.length === 0) return <EmptyList />;
+    return (
+      <section className='pt-24'>
+        <SectionTitle text='featured products' />
+        <ProductsGrid products={products} />
+      </section>
+    );
 }
-
-export default FeaturedProducts
+export default FeaturedProducts;
