77577a62d103229117386cf2b9ab9708e6eb0dbe
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2025-01-27 16:04:57 +0900

    search ok

--- utils/actions.ts
+++ utils/actions.ts
@@ -9,11 +9,16 @@ export const fetchFeaturedProducts = async () => {
   return products;
 };
 
-export const fetchAllProducts = () => {
-    return db.product.findMany({
-      orderBy: {
-        createdAt: 'desc',
-      },
-    });
-  };
-  
+export const fetchAllProducts = ({ search = '' }: { search: string }) => {
+  return db.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { company: { contains: search, mode: 'insensitive' } },
+      ],
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+};
